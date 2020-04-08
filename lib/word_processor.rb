11108843229684c7a68b3973a7161561d04b84aa
kealanheena <kealanheena@gmail.com>
2020-04-08 17:27:46 +0100

    Chore: Add test for cat

--- lib/word_processor.rb
+++ lib/word_processor.rb
@@ -1,11 +1,19 @@
-class WordProcessor
+# frozen_string_literal: true
 
+# A word processor for spell checking words
+class WordProcessor
   def spell_checker(words)
-    if words === 'hello' || words === 'goodbye' || words === ''
+    case words
+    when 'hello'
+      words
+    when 'goodbye'
+      words
+    when 'cat'
+      words
+    when ''
       words
     else
       "~#{words}~"
     end
   end
-
 end
--- spec/word_processor_spec.rb
+++ spec/word_processor_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'word_processor'
 
 describe WordProcessor do
   describe '#spell_checker' do
     it 'should take a string and return a string' do
-      expect(subject.spell_checker('')).to eq '' 
+      expect(subject.spell_checker('')).to eq ''
     end
 
-    it 'should return the word or phrase unaltered if there is no spelling mistakes' do
-      expect(subject.spell_checker('hello')).to eq 'hello' 
+    it 'returns the word or phrase unaltered if there is no spelling mistake' do
+      expect(subject.spell_checker('hello')).to eq 'hello'
     end
 
     it 'should return misspelt word with "~" before and after the word' do
@@ -20,6 +22,10 @@ describe WordProcessor do
 
     it 'should put "~" before and after "godbye"' do
       expect(subject.spell_checker('godbye')).to eq '~godbye~'
+    end
+
+    it 'should work for "cat"' do
+      expect(subject.spell_checker('cat')).to eq 'cat'
     end
   end
 end
