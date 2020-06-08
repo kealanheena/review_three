725084f295b5d309a747ec1576bd245b0c8b9cd4
kealanheena <kealanheena@gmail.com>
2020-06-08 14:58:34 +0100

    Chore: split calculate function into smaller functions

--- lib/review.rb
+++ lib/review.rb
@@ -1,20 +1,34 @@
  
  class StringCalculator
+
   def calculate(string)
     return ['', 0] if string == ''
-
+    
     equation_array = string.split(' ')
-    sum = equation_array[0].to_i
-    if equation_array[1] == '+'
-      sum = equation_array[0].to_i + equation_array[2].to_i
-    elsif equation_array[1] == '-'
-      sum = equation_array[0].to_i - equation_array[2].to_i
-    elsif equation_array[1] == '*'
-      sum = equation_array[0].to_i * equation_array[2].to_i
-    elsif equation_array[1] == '/'
-      sum = equation_array[0].to_i / equation_array[2].to_i
-    end
+    @sum = equation_array[0].to_i
+    operator = get_operator(equation_array)
+    equation_array = equation_array.map { |number| number.to_i }
 
-    [string, sum]
+    calculate_with_operator(operator, equation_array)
+
+    [string, @sum]
+  end
+
+  private
+
+  def get_operator(array)
+    array.delete_at(1)
+  end
+
+  def calculate_with_operator(operator, array)
+    if operator == '+'
+      @sum = array.reduce(:+)
+    elsif operator == '-'
+      @sum = array.reduce(:-)
+    elsif operator == '*'
+      @sum = array.reduce(:*)
+    elsif operator == '/'
+      @sum = array.reduce(:/)
+    end
   end
  end
