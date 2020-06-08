3fc7c7220c409e5ff9130c98ccc7cfec99ac04cc
kealanheena <kealanheena@gmail.com>
2020-06-08 14:24:18 +0100

    Chore: add method for adding string with an operator

--- lib/review.rb
+++ lib/review.rb
@@ -1,6 +1,12 @@
  class StringCalculator
   def calculate(string)
     return ['', 0] if string == ''
-    [string, string.to_i]
+    equation_array = string.split(' ')
+    sum = equation_array[0].to_i
+    if equation_array[1] == '+'
+      sum = equation_array[0].to_i + equation_array[2].to_i
+    end
+
+    [string, sum]
   end
  end
--- spec/review_spec.rb
+++ spec/review_spec.rb
@@ -3,16 +3,29 @@ require 'review.rb'
 
 describe StringCalculator do
   describe '#calculate' do
-    it 'should return an array' do
+    it 'should return an array with an empy string and 0 if and empty string isd passed' do
       expect(subject.calculate('')).to eq ['', 0]
     end
 
-    it 'should return an array with two 1\'s if 1 is inputed' do
-      expect(subject.calculate('1')).to eq ['1', 1]
+    context 'if there is no operator passed' do
+
+      it 'should return an array with two 1\'s if 1 is inputed' do
+        expect(subject.calculate('1')).to eq ['1', 1]
+      end
+
+      it 'should return an array with two 2\'s if 2 is inputed' do
+        expect(subject.calculate('2')).to eq ['2', 2]
+      end
+
+      it 'should return an array with two 3\'s if 3 is inputed' do
+        expect(subject.calculate('3')).to eq ['3', 3]
+      end
     end
 
-    it 'should return an array with two 2\'s if 2 is inputed' do
-      expect(subject.calculate('2')).to eq ['2', 2]
+    context 'if there is an operator passed' do
+      it 'should return an array' do
+        expect(subject.calculate('1 + 1')).to eq ['1 + 1', 2]
+      end
     end
   end
 end
